38b96b2898454e3263c9d0323d1a9fc00acde143
arpitrana717 <arpitrana.underpinservices@gmail.com>
2024-09-09 12:19:06 +0530

    webpack Sound added

--- public/config.prod.js
+++ public/config.prod.js
@@ -95,6 +95,7 @@ module.exports = {
         new CopyPlugin({
             patterns: [
                 { from: 'public/src/sprites', to: 'src/sprites' },
+                { from: 'public/src/sounds', to: 'src/sounds' },
                 { from: 'public/favicon.png', to: 'favicon.png' },
                 { from: 'public/style.css', to: 'style.css' }
             ],
